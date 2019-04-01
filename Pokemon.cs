8061210c616a96f24bb353237f19bed7a24a2113
KlamydiaKenneth <43172529+KlamydiaKenneth@users.noreply.github.com>
2019-04-01 14:51:27 +0200

    Pokemon Assignment

---
+++ Pokemon.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    /// <summary>
+    /// The possible elemental types
+    /// </summary>
+    public enum Elements
+    {
+        Fire,
+        Water,
+        Grass
+    }
+
+    public class Pokemon
+    {
+        //fields
+        int level;
+        int baseAttack;
+        int baseDefence;
+        float hp;
+        float maxHp;
+        Elements element;
+
+        //properties, imagine them as private fields with a possible get/set property (accessors)
+        //in this case used to allow other objects to read (get) but not write (no set) these variables
+        public string Name { get; }
+        //example of how to make the string Name readable AND writable  
+        //  public string Name { get; set; }
+        public List<Move> Moves { get; }
+        //can also be used to get/set other private fields
+        public float Hp { get => hp; set => hp = maxHp; }
+        public float maxHP { get => maxHp; set => hp = maxHp; }
+
+        /// <summary>
+        /// Constructor for a Pokemon, the arguments are fairly self-explanatory
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="level"></param>
+        /// <param name="baseAttack"></param>
+        /// <param name="baseDefence"></param>
+        /// <param name="hp"></param>
+        /// <param name="element"></param>
+        /// <param name="moves">This needs to be a List of Move objects</param>
+        public Pokemon(string name, string type, int level, int baseAttack,
+            int baseDefence, int hp, Elements element,
+            List<Move> moves)
+        {
+            this.level = level;
+            this.baseAttack = baseAttack;
+            this.baseDefence = baseDefence;
+            this.Name = name;
+            this.hp = hp * this.level;
+            this.maxHp = hp * this.level;
+            this.element = element;
+            this.Moves = moves;
+        }
+
+        /// <summary>
+        /// performs an attack and returns total damage, check the slides for how to calculate the damage
+        /// IMPORTANT: should also apply the damage to the enemy pokemon
+        /// </summary>
+        /// <param name="enemy">This is the enemy pokemon that we are attacking</param>
+        /// <returns>The amount of damage that was applied so we can print it for the user</returns>
+        public float Attack(Pokemon enemy)
+        {
+            float attackdamage = ((this.baseAttack * this.level)  - enemy.CalculateDefence()) * CalculateElementalEffects(enemy.element);
+
+            //Applying damage to the attacked pokemon.
+            if (attackdamage < 1)
+            {
+                attackdamage = 1;
+            }
+
+            enemy.hp -= attackdamage;
+
+            if (enemy.hp < 0)
+            {
+                enemy.hp = 0;
+            }
+
+            return attackdamage;
+        }
+
+        /// <summary>
+        /// calculate the current amount of defence points
+        /// </summary>
+        /// <returns> returns the amount of defence points considering the level as well</returns>
+        public int CalculateDefence()
+        {
+            int defence = this.baseDefence * this.level;
+
+            return defence;
+        }
+
+        /// <summary>
+        /// Calculates elemental effect, check table at https://bulbapedia.bulbagarden.net/wiki/Type#Type_chart for a reference
+        /// </summary>
+        /// <param name="damage">The amount of pre elemental-effect damage</param>
+        /// <param name="enemyType">The elemental type of the enemy</param>
+        /// <returns>The damage post elemental-effect</returns>
+        /// 
+        /// I Could not find the sense in calculating from the pre elemental damage,
+        /// since it's a multiplier. So i instead made it in to a simple multiplier
+        /// dependant on the enemies and your own pokemons Element.
+        public float CalculateElementalEffects(Elements enemyType)
+        {
+            float elementalMultiplier = 0;
+
+            if (this.element == Elements.Fire)
+            {
+                if (enemyType == Elements.Fire)
+                {
+
+                    elementalMultiplier = 1;
+                }
+                if (enemyType == Elements.Grass)
+                {
+
+                    elementalMultiplier = 2;
+                }
+                if (enemyType == Elements.Water)
+                {
+
+                    elementalMultiplier = .5f;
+                }
+            }
+
+            else if (this.element == Elements.Water)
+            {
+                if (enemyType == Elements.Fire)
+                {
+
+                    elementalMultiplier = 2;
+                }
+                if (enemyType == Elements.Grass)
+                {
+
+                    elementalMultiplier = .5f;
+                }
+                if (enemyType == Elements.Water)
+                {
+
+                    elementalMultiplier = 1f;
+                }
+            }
+
+            else if (this.element == Elements.Grass)
+            {
+                if (enemyType == Elements.Fire)
+                {
+
+                    elementalMultiplier = .5f;
+                }
+                if (enemyType == Elements.Grass)
+                {
+
+                    elementalMultiplier = 1;
+                }
+                if (enemyType == Elements.Water)
+                {
+
+                    elementalMultiplier = 2;
+                }
+            }
+
+            return elementalMultiplier;
+        }
+
+        /// <summary>
+        /// Applies damage to the pokemon
+        /// </summary>
+        /// <param name="damage"></param>
+        /// This seemed to me like an extra step, that wasnt really important to apply the damage.
+        /// instead i applied it in the Attack() function.
+        public void ApplyDamage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Heals the pokemon by resetting the HP to the max
+        /// </summary>
+        public void Restore()
+        {
+            hp = maxHp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> roster = new List<Pokemon>();
+            List<Move> Grass = new List<Move>();
+            List<Move> Fire = new List<Move>();
+            List<Move> Water = new List<Move>();
+
+            //Assigning the different moves according to pokemon type
+            Grass.Add(new Move("Cut"));
+            Grass.Add(new Move("Razor Leaf"));
+
+            Fire.Add(new Move("Ember"));
+            Fire.Add(new Move("Fire Blast"));
+
+            Water.Add(new Move("Bubble"));
+            Water.Add(new Move("Bite"));
+
+            // INITIALIZE YOUR THREE POKEMONS HERE
+            //For balance reasons i adjusted Both Bulbasaur and Squirtles defense to 46 and 45 respectively, so we sont get 50+ turn games.
+            roster.Add(new Pokemon("Bulbasaur", "Grass", 5, 49, 46, 45, Elements.Grass, Grass));
+            roster.Add(new Pokemon("Charmander", "Fire", 5, 52, 43, 39, Elements.Fire, Fire));
+            roster.Add(new Pokemon("Squirtle", "Water", 5, 50, 45, 44, Elements.Water, Water));
+
+            Console.WriteLine("Welcome to the world of Pokemon!\nThe available commands are list/fight/heal/quit \nlist shows you the available" +
+                " Pokemon, \nfight starts the battle and makes you choose which pokemon are going to fight, \nheal will heal all the pokemon currently" +
+                "not in combat (used after a victory or defeat to play again), \nquit makes you quit the application.");
+
+            while (true)
+            {
+                Console.WriteLine("\nPlese enter a command");
+                switch (Console.ReadLine())
+                {
+                    case "list":
+                        // PRINT THE POKEMONS IN THE ROSTER HERE
+                        Console.WriteLine("Available Pokemon:");
+                        foreach (Pokemon pokemon in roster)
+                        {
+                            Console.WriteLine(pokemon.Name);
+                        }
+                        break;
+
+                    case "fight":
+                        //PRINT INSTRUCTIONS AND POSSIBLE POKEMONS (SEE SLIDES FOR EXAMPLE OF EXECUTION)
+                        Console.Write("Choose who should fight.\nWrite the Pokemons names with a single space between them example: Squirtle Charmander" +
+                            "\nThe first Pokemon is the one you will be controlling, the second one is the opponent." +
+                            "\nYou can't choose the same Pokemon for each team." +
+                            "\n\nPokemons to choose from:\n");
+
+                        foreach (Pokemon pokemon in roster)
+                        {
+                            Console.WriteLine(pokemon.Name);
+                        }
+
+                        //READ INPUT, REMEMBER IT SHOULD BE TWO POKEMON NAMES
+                        string input = Console.ReadLine();
+                        string[] chosenPokemons;
+                        char[] splitchar = { ' ' };
+
+                        chosenPokemons = input.Split(splitchar);
+                        Console.WriteLine(chosenPokemons);
+
+                        //BE SURE TO CHECK THE POKEMON NAMES THE USER WROTE ARE VALID (IN THE ROSTER) AND IF THEY ARE IN FACT 2!
+                        Pokemon player = null;
+                        Pokemon enemy = null;
+
+                        if (chosenPokemons.Count() == 2)
+                        {
+                            if (chosenPokemons.ElementAt(0) == "Bulbasaur")
+                            {
+                                player = roster.ElementAt(0);
+                            }
+
+                            else if (chosenPokemons.ElementAt(0) == "Charmander")
+                            {
+                                player = roster.ElementAt(1);
+                            }
+
+                            else if (chosenPokemons.ElementAt(0) == "Squirtle")
+                            {
+                                player = roster.ElementAt(2);
+                            }
+
+                            else
+                            {
+                                player = null;
+                            }
+
+                            if (chosenPokemons.ElementAt(1) == "Bulbasaur")
+                            {
+                                enemy = roster.ElementAt(0);
+                            }
+
+                            else if (chosenPokemons.ElementAt(1) == "Charmander")
+                            {
+                                enemy = roster.ElementAt(1);
+                            }
+
+                            else if (chosenPokemons.ElementAt(1) == "Squirtle")
+                            {
+                                enemy = roster.ElementAt(2);
+                            }
+
+                            else
+                            {
+                                enemy = null;
+                            }
+                        }
+
+                        
+
+                        //if everything is fine and we have 2 pokemons let's make them fight
+                        if (player != null && enemy != null && player != enemy)
+                        {
+                            Console.WriteLine("A wild " + enemy.Name + " appears!");
+                            Console.Write(player.Name + " I choose you! \n");
+
+                            //BEGIN FIGHT LOOP
+                            while (player.Hp > 0 && enemy.Hp > 0)
+                            {
+                                //PRINT POSSIBLE MOVES
+                                Console.Write("\nWhat move should we use?\n");
+
+                                int count = 0;
+                                foreach (Move ability in player.Moves)
+                                {
+                                    Console.WriteLine("[" + count + "] " + ability.Name);
+                                    count++;
+                                }
+
+                                //GET USER ANSWER, BE SURE TO CHECK IF IT'S A VALID MOVE, OTHERWISE ASK AGAIN
+                                int move = -1;
+                                switch (Console.ReadLine())
+                                {
+                                    case "0":
+                                        move = 0;
+                                        break;
+
+                                    case "1":
+                                        move = 1;
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("Invalid Move");
+                                        break;
+                                
+                                }
+                                //CALCULATE AND APPLY DAMAGE
+                                float damage = player.Attack(enemy);
+
+                                //print the move and damage
+                                Console.WriteLine(player.Name + " uses " + player.Moves[move].Name + ". " + enemy.Name + " loses " + damage + " HP");
+                                Console.WriteLine(enemy.Name + " HP: " + enemy.Hp + "/" + enemy.maxHP);
+
+                                //if the enemy is not dead yet, it attacks
+                                if (enemy.Hp > 0)
+                                {
+                                    //CHOOSE A RANDOM MOVE BETWEEN THE ENEMY MOVES AND USE IT TO ATTACK THE PLAYER
+                                    Random rand = new Random();
+                                    /*the C# random is a bit different than the Unity random
+                                     * you can ask for a number between [0,X) (X not included) by writing
+                                     * rand.Next(X) 
+                                     * where X is a number 
+                                     */
+                                    int enemyMove = -1;
+
+                                    int randomAttack;
+                                    randomAttack = rand.Next(0, 2);
+                                    if (randomAttack == 0)
+                                    {
+                                        enemyMove = 0;
+                                    }
+
+                                    else if (randomAttack == 1)
+                                    {
+                                        enemyMove = 1;
+                                    }
+
+                                    float enemyDamage = enemy.Attack(player);
+
+                                    //print the move and damage
+                                    Console.WriteLine(enemy.Name + " uses " + enemy.Moves[enemyMove].Name + ". " + player.Name + " loses " + enemyDamage + " HP");
+                                    Console.WriteLine(player.Name + " HP: " + player.Hp + "/" + player.maxHP);
+                                }
+                            }
+                            //The loop is over, so either we won or lost
+                            if (enemy.Hp <= 0)
+                            {
+                                Console.WriteLine(enemy.Name + " faints, you won!" +
+                                    "\n" + player.Name + " HP: " + player.Hp + "/" + player.maxHP);
+                            }
+                            else
+                            {
+                                Console.WriteLine(player.Name + " faints, you lost..." +
+                                    "\n" + enemy.Name + " HP: " + enemy.Hp + "/" + enemy.maxHP);
+                            }
+                        }
+                        //otherwise let's print an error message
+                        else
+                        {
+                            Console.WriteLine("Invalid pokemons");
+                        }
+                        break;
+
+                    case "heal":
+                        //RESTORE ALL POKEMONS IN THE ROSTER
+                        foreach (Pokemon pokemon in roster)
+                        {
+                            pokemon.Hp = pokemon.maxHP;
+                        }
+
+                        Console.WriteLine("All pokemons have been healed");
+                        break;
+
+                    case "quit":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown command");
+                        break;
+                }
+            }
+        }
+    }
+}
